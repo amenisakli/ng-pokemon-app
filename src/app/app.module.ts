4ea67c44dd50d6df8db5bb4263ea06974064daed
ameni <amenysakly@gmail.com>
2024-05-15 11:19:17 +0100

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { BorderCardDirective } from "./border-card.directive";
 import { TitleCardDirective } from "./title-card.directive";
 import { SizeImgDirective } from './size-img.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, BorderCardDirective, TitleCardDirective, SizeImgDirective, PokemonTypeColorPipe],
+  declarations: [AppComponent, BorderCardDirective, TitleCardDirective, SizeImgDirective, PokemonTypeColorPipe, ListPokemonComponent, DetailPokemonComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
